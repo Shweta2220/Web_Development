e8e99a71ae5500b7b2072207cab6fec5e64e6919
shweta2220 <shwetabpawar3@gmail.com>
2023-03-20 14:25:48 +0530

    js

---
+++ JS/Assignment 09/3.SumOfElementsArr.js
@@ -0,0 +1,8 @@
+let Arr=[2,4,6,8,2,4,6,8];
+var add=0;
+for(let i=0;i<=Arr.length-1;i++)
+{
+    add=add+Arr[i];
+    console.log(add);
+}
+console.log(`Sum of Array Elements=${add}`);
---
+++ JS/Assignment 09/4.EvenNoOfGivenArr.js
@@ -0,0 +1,9 @@
+let Arr = [2,3,4,7,9,8,8];
+
+for(i=0;i<Arr.length;i++)
+{
+    if(Arr[i]%2===0)
+    {
+        console.log(Arr[i]);
+    }
+}
