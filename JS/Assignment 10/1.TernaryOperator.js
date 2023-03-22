e33c1bce465dc610182622b34e60a42f9574a5eb
shweta2220 <shwetabpawar3@gmail.com>
2023-03-22 21:14:08 +0530

    UsingWhileLoop

---
+++ JS/Assignment 10/1.TernaryOperator.js
@@ -0,0 +1,8 @@
+var No1=35,No2=96;
+
+No1>No2?console.log(`Maximum No is=`,No1):console.log(`Maximum No is=`,No2);
+
+/*  Or 
+
+var Max= No1>No2?No1:No2;
+console.log(`Maximum No is=`,Max); */
---
+++ JS/Assignment 10/3.2.whileloopWEBx86_Infotech.js
@@ -0,0 +1,6 @@
+var i=1;
+while(i<=10)
+{
+    console.log(i,`.\t WEBx86 \n \tInfotech`);
+    i++;
+}
