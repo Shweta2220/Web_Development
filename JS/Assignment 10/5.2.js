28e6de68cbb04d522e7717a9ca412b1054a586df
shweta2220 <shwetabpawar3@gmail.com>
2023-04-07 18:11:08 +0530

    switchcase

---
+++ JS/Assignment 10/5.2.js
@@ -0,0 +1,20 @@
+const InputtedNumbers = [12, 56, 755, 27, 16, 2, 1104, 65, 88];
+for(let i in InputtedNumbers)
+{
+    switch(true)
+    {
+        case InputtedNumbers[i] % 3 === 0 && InputtedNumbers[i] % 8 === 0:
+            console.log(InputtedNumbers[i]  + " " + "divisible by 3 And 8");
+            break;
+        case InputtedNumbers[i] % 3 === 0:
+            console.log(InputtedNumbers[i]  + " " + "divisible by 3");
+            break;
+        case InputtedNumbers[i] % 8 === 0:
+            console.log(InputtedNumbers[i]  + " " + "divisible by 8");
+            break;
+        default:
+            console.log(InputtedNumbers[i]  + " " + "Not Divisible by 3 And 8");
+            break;
+    }
+ 
+}
