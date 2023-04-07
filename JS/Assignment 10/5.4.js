677cb4fcf77a722608e426197b32e1b9506b18cf
shweta2220 <shwetabpawar3@gmail.com>
2023-04-07 18:14:34 +0530

    forLoopwithswitchcase

---
+++ JS/Assignment 10/5.4.js
@@ -0,0 +1,21 @@
+const InputtedNumbers = [12, 56, 755, 27, 16, 2, 1104, 65, 88];
+
+for(let i of InputtedNumbers)
+{
+    switch(true)
+    {
+        case i % 3 === 0 && i % 8 === 0:
+            console.log(i  ,"divisible by 3 And 8");
+            break;
+        case i % 3 === 0:
+            console.log(i , "divisible by 3");
+            break;
+        case i % 8 === 0:
+            console.log(i , "divisible by 8");
+            break;
+        default:
+            console.log(i , "Is Not Divisible by 3 And 8");
+            break;
+    }
+ 
+}
